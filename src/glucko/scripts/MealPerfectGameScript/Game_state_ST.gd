fb10c778e4290a35fc6f84edc7712f384211f71d
michele232 <michelepetruzzelli24@icloud.com>
2026-03-13 17:31:38 +0100

    Sistemazione finale

--- src/glucko/scripts/MealPerfectGameScript/Game_state_ST.gd
+++ src/glucko/scripts/MealPerfectGameScript/Game_state_ST.gd
@@ -1,28 +1,28 @@
 extends Node
-#classe autoload che gestisce lo stato del gioco durante la partita,
-#emettendo un segnale quando il livello è completato
+
 signal game_won
-
-const WIN_SCORE := 3#numero di 'piatti perfetti' per far vincere il player
-
+const WIN_SCORE := 1
 var perfect_plates: int = 0
 var game_over: bool = false
 
 func register_perfect_plate() -> void:
 	if game_over:
 		return
-
 	perfect_plates += 1
-
 	if perfect_plates >= WIN_SCORE:
 		game_over = true
+		_remove_mobile_controls()
 		game_won.emit()
-
 
 func reset_game() -> void:
 	perfect_plates = 0
 	game_over = false
 
+func _remove_mobile_controls() -> void:
+	for child in get_tree().root.get_children():
+		if child.scene_file_path == "res://scenes/MealPerfectgameScenes/mobile_controls_pm.tscn":
+			child.queue_free()
+			break
 
 func _on_btn_exit_mpg_pressed() -> void:
-		get_tree().change_scene_to_file("res://scenes/menus/glucky/Minigame_Selection.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/glucky/Minigame_Selection.tscn")
