b15abac33fc66dc2dd2f4d26d38099f08ad1324b
Vincenzoera <vincepioeracleagmail.com>
2026-03-17 20:11:34 +0100

    correzione gamereset in MPG , final commit2

--- src/glucko/scripts/MealPerfectGameScript/btn_exit_msin_scene.gd
+++ src/glucko/scripts/MealPerfectGameScript/btn_exit_msin_scene.gd
@@ -4,6 +4,7 @@ func _ready():
 	pressed.connect(_on_pressed)
 
 func _on_pressed():
+	GameState.reset_game() #reset del gioco se si esce da un qualsiasi livello per poi ricominciaree
 	for child in get_tree().root.get_children():
 		if child.scene_file_path == "res://scenes/MealPerfectgameScenes/mobile_controls_pm.tscn":
 			child.queue_free()
